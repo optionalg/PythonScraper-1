ca289b92784e9698580cdb8e2742b3eeef20cce0
wraley <bill@ctcosys.com>
2016-10-09 16:02:07 -0400

    Add files via upload

---
+++ scraper.py
@@ -0,0 +1,103 @@
+
+from urllib.error import HTTPError
+from bs4 import BeautifulSoup
+import re
+from urllib.request import Request, urlopen
+import xlrd
+import xlsxwriter
+
+#url = 'http://www.metacritic.com/game/pc/overwatch'
+#response = requests.get(url)
+#html = response.content
+
+def clean_titles(title):
+    clean_title = title.replace('--', '')
+    clean_title = clean_title.replace('/', '-')
+    clean_title = clean_title.replace('  ',' ')
+    clean_title = clean_title.replace(':','')
+    clean_title = clean_title.replace("'", '')
+    clean_title = clean_title.replace(' ', '-')
+    clean_title = clean_title.replace('"', '')
+    clean_title = clean_title.replace("[text",'')
+    clean_title = clean_title.lower()
+    return clean_title
+
+def build_urls(titles_fixed):
+    base_url = 'http://www.metacritic.com/game/pc/'
+    url = ''
+    url_list = []
+
+    for titles in titles_fixed:
+        url = base_url + titles
+        url_list.append(url)
+
+    return url_list
+
+def has_game_rating(tag):
+    return tag.has_attr('itemprop')==re.compile('ratingValue')
+
+
+rdbook = xlrd.open_workbook('gamedata.xlsx')
+wrtbook = xlsxwriter.Workbook('Metacritic.xlsx')
+metaData = wrtbook.add_worksheet()
+ignData = rdbook.sheet_by_name('Sheet3')
+#metaData = workbook.add_sheet('Sheet6')
+
+game_list = []
+game_pair = []
+game_info = ''
+game_titles = []
+titles_fixed = []
+url_list = []
+score_list = []
+
+for rows in range(ignData.nrows):
+    game_info = ignData.row(rows)
+    game_list.append(game_info)
+print(game_list)
+
+for games in game_list:
+    game_pair = str(games).split(',')
+    game_titles.append(game_pair[0])
+#print(game_titles)
+
+for titles in game_titles:
+    titles_fixed.append(clean_titles(str(titles)))
+#print(titles_fixed)
+
+url_list = build_urls(titles_fixed)
+print(url_list)
+
+valid_page = True
+
+# metacritic parser
+for urls in url_list:
+    #req = Request('http://www.metacritic.com/game/pc/world-of-warcraft-legion', headers={'User-Agent': 'Mozilla/5.0'})
+    valid_page = True
+    try:
+        req = Request(urls, headers={'User-Agent': 'Mozilla/5.0'})
+        html = urlopen(req).read()
+    except HTTPError:
+        valid_page = False
+        #print('failed')
+
+    if(valid_page):
+        soup = BeautifulSoup(html, 'html.parser')
+        tag = str(soup.find(itemprop=re.compile('ratingValue')))
+        tag = tag.replace('<span itemprop="ratingValue">', '')
+        tag = tag.replace("</span>", "")
+        score_list.append(tag)
+    else:
+        score_list.append('failed')
+
+
+
+row = 0
+
+for score in score_list:
+    metaData.write(row,0,game_titles[row])
+    metaData.write(row, 4,score)
+    row += 1
+
+wrtbook.close()
+print('success!')
